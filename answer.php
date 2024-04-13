0085db8cf3b9edde53cb23bb1de196fa95dd8802
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-13 17:15:38 +0000

    formatted

--- answer.php
+++ answer.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="utf-8" />
   <meta name="description" content="Area of a Trapezoid, in PHP" />
@@ -16,7 +15,6 @@
   <link rel="manifest" href="site.webmanifest" />
   <title>Area of a Trapezoid, in PHP</title>
 </head>
-
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
